8c8810d3af7dcba0f26d3178b12d099d635d263d
MarianBaba <marianbaba16@gmail.com>
2025-01-02 17:29:09 +0100

    upd

---
+++ data_structures/graph/graph.c
@@ -0,0 +1,14 @@
+
+struct node {
+    int key;
+    struct node *next;
+};
+
+struct graph {
+    struct node **vertices; // -> adjacency list
+    // node adj[][10]; -> adjacency matrix
+};
+
+int main() {
+
+}
